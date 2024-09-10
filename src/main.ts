3ec187f168fd96d41a1f21ee92aa99607c82b401
Joohyun Kim <corinthionia@gmail.com>
2024-09-11 00:50:05 +0900

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('ReadME API DOCS')
+    .setDescription('팀 맹모닝불여일견')
+    .setVersion('1.0')
+    .addServer('http://localhost:8080')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
